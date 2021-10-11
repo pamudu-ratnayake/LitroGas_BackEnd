de7e113bf0b68999beca831b35579149e87828c1
oshini-98 <it19990446@my.sliit.lk>
2021-10-11 08:05:30 +0530

    Add Update Dealer

--- src/controllers/dealerCT.js
+++ src/controllers/dealerCT.js
@@ -7,14 +7,12 @@ const dealerPost = (req, res, next) => {
     const shop = req.body.shop;
     const address = req.body.address;
     const tell = req.body.tell;
-    const distance = req.body.distance;
 
     const newDealer = new dealer ({
         name,
         shop,
         address,
         tell,
-        distance
     });
 
     newDealer.save().then(() => {
@@ -24,6 +22,54 @@ const dealerPost = (req, res, next) => {
     });
 };
 
+const dealerUpdate = (req, res, next) => {
+    let ID = req.params.id;
+    const {
+        name,
+        shop,
+        address,
+        tell,
+    } = req.body;
+
+    const updateDealer = {
+        name,
+        shop,
+        address,
+        tell
+    };
+
+    const update = dealer.findByIdAndUpdate(ID,updateDealer).then(() => {
+        res.status(200).send({status: "Dealer Updated", user: update});
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send({status: "Error with updating data"});
+    });
+};
+
+// const dealerDelete = (req, res, next) => {
+//     let ID =req.params.id;
+//     dealer.findByIdAndDelete(ID).then(() => {
+//         res.status(200).send({status: "Dealer Deleted"});
+//     }).catch((err) => {
+//         console.log(err.message);
+//         res.status(500).send({ status: "Error with delete user", error: err.message})
+//     });
+// };
+
+// const dealerGet = (req, res, next) => {
+//     let ID = req.params.id;
+
+//     const dealerInfor = dealer.findById(ID).then((dealerData) => {
+//         res.json(dealerData);
+//     }).catch((err) => {
+//         console.log(err.message);
+//         res.status(500).send({ status: "Error with get user", error: err.message});
+//     });
+// };
+
 module.exports = {
     dealerPost,
+    dealerUpdate,
+    // dealerDelete,
+    // dealerGet
 };
--- src/routes/dealerRT.js
+++ src/routes/dealerRT.js
@@ -3,4 +3,10 @@ const dealerController = require("../../src/controllers/dealerCT");
 
 router.post("/addDealer", dealerController.dealerPost);
 
+router.put("/updateDealer/:id", dealerController.dealerUpdate);
+
+// router.delete("/deleteDealer/:id", dealerController.dealerDelete);
+
+// router.get("/getDealer/:id", dealerController.dealerGet);
+
 module.exports = router;
